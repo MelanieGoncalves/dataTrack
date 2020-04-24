a5aebb8de5e9f31bb8f3f0f313d5fd6c03eb54d5
Melanie Goncalves <melaniegoncalves5@gmail.com>
2020-04-23 23:59:32 -0700

    test

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/api/name', (req, res) => {
 }); */
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, './client/build')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
