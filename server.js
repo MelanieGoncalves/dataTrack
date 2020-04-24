c993b4ac500879187d6a45e7e045ca255c365d1d
Melanie Goncalves <melaniegoncalves5@gmail.com>
2020-04-23 23:49:32 -0700

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8080;
 const ROUTER = express.Router();
 const USERROUTES = require('./routes/user');
 
-//const routes = require('./routes');
+const routes = require('./routes');
 
 //const MONGODB_URI = require('./config_uri.js').mongoURI;
 
@@ -29,7 +29,7 @@ mongoose.connection.on('connected', () => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(USERROUTES, ROUTER);
-//app.use('/', routes);
+app.use('/', routes);
 
 /* app.get('/', (req, res) => {
     const data = {
