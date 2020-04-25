b338303c70cab0b585a9881f14947c1046ae0bee
Melanie Goncalves <melaniegoncalves5@gmail.com>
2020-04-25 11:27:43 -0700

    images

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use('/', routes);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, './client/build')));
-
+    app.use(express.static(path.join(__dirname, '/client/src/images')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
